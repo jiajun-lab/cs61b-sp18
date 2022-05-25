a1522adfaa8fdffab86827e309deac2b1461da0a
李佳骏 <73824503+jiajun-lab@users.noreply.github.com>
2022-05-25 20:18:51 +0800

    finish Lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList p = A;
+        while (p.rest!=null){
+            p = p.rest;
+        }
+        p.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,21 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList head = new IntList(A.first,null);
+        IntList p = head;
+        while(A.rest!=null){
+            A = A.rest;
+            p.rest = new IntList(A.first,null);
+            p = p.rest;
+        }
+        p.rest = new IntList(B.first,null);
+        p = p.rest;
+        while(B.rest!=null){
+            B = B.rest;
+            p.rest = new IntList(B.first,null);
+            p = p.rest;
+        }
+        return head;
     }
 
 
